474b4b82c215ba7e5c50918d5a46de0d2908093e
Samuelcodingg <samuelroman39@gmail.com>
2021-09-12 18:45:38 -0500

    model Todo added

---
+++ TodoListMvc/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo_List_MVC.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Write a todo please.")]
+        [StringLength(20, ErrorMessage = "The lenght must be between {2} and {1}", MinimumLength = 5)]
+        public string Description { get; set; }
+    }
+}
